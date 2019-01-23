53e90d620d49adfbdb470daa6bd4b5409f5a758e
ZhangHe <zhanghe0309@qq.com>
2019-01-23 21:55:59 +0800

    Create demo4.py

---
+++ A-Byte-Of-Python/demo4.py
@@ -0,0 +1,19 @@
+#文件读写
+
+poem = '''\
+Programming is fun
+When the work is done
+if you wanna make your work also fun:
+    use Python!
+'''
+
+f = open('poem.txt','w')
+
+f.write(poem)
+
+f.close()
+
+f = open('poem.txt')
+
+while True:
+    line = f.readline()
